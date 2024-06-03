31e21d284f52232cd92cbb8c8ababc07e20430ab
tomas szyszkowicz <tomasszyszkowicz@email.cz>
2024-06-03 23:14:47 +0200

    styled the pagination

--- src/components/HobbiesDiv.jsx
+++ src/components/HobbiesDiv.jsx
@@ -36,29 +36,29 @@ const HobbiesDiv = ({ isVisible }) => {
     return (
         <div className={`content-div-secondary ${isMoved ? 'moved4' : ''}`}>
             <h1>Hobbies</h1>
-            <button onClick={() => move('left')}>Index +</button>
-            <button onClick={() => move('right')}>Index -</button>
             <InnerContentDiv index={indexes[0]}>
-                <p>tomasektomasektomasektomasektomasektomasek
-                tomasektomasektomasektomasektomasektomasek
-                tomasektomasektomasektomasektomasektomasek
-                tomasektomasektomasektomasektomasektomasek
+                <h2>Speedcubing</h2>
+                <p>
+                    I'm speedcuber since 2016. Since then I've been to many competitions Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae excepturi aspernatur obcaecati hic cumque voluptates atque et consequatur dicta repudiandae ipsum, aperiam possimus provident, accusamus ad. Iste enim ratione ea.
                 </p>
             </InnerContentDiv>
             <InnerContentDiv index={indexes[1]}>
-                <p>chujovychujchujovychujchujovychuj faf fafafda
-                     fadfa dafdasfugyagufd hgfaghuf
-                     dahgf guydg gdg dhgadg ghda'
-                     udyafg yugdauygdfadasuyg 
+                <h2>Video games</h2>
+                <p>
+                    I'm a big fan of video games. I play games since I was a little kid. My favourite games are: The Witcher 3, Dark Souls 3, Sekiro, and many more.
                 </p>
             </InnerContentDiv>
             <InnerContentDiv index={indexes[2]}>
-                <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-                    xxxxxxxxxxxxxxxxxxxxxxxxxx
+                <h2>Speedtyping</h2>
+                <p>
+                    I'm also a speedtyper. I can type around 100 words per minute. I like to practice my typing skills on websites like 10fastfingers or monkeytype.
                 </p>
             </InnerContentDiv>
+            <div className="navigation-div">
+                <button onClick={() => move('right')} className="header-button" style={{border: 'solid 1px white', margin: '10px'}}>⟨</button>
+                <button onClick={() => move('left')} className="header-button" style={{border: 'solid 1px white', margin: '10px'}}>⟩</button>
+            </div>
+            
         </div>
     );
 };
